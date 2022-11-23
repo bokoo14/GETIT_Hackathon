0598669e6934017ab4650e521be17a6eceba1752
JMEspoir <maro3534@knu.ac.kr>
2022-11-23 22:10:07 +0900

    List

--- daegu/src/App.js
+++ daegu/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import styled from "styled-components";
 import { css } from "styled-components";
+import NotificationList from "./components/list/Notificationlist";
 
 const Div = styled.div`
   overflow: hidden;
   font-family: "Roboto Slab", serif;
-  background-color: #bbdefb;
+  background-color: #F5EDDD;
 `;
 
 const WelcomeDiv = styled.div`
@@ -139,7 +140,8 @@ const Logo = styled.h3`
   font-family: "Hallym";
   font-weight: bold;
 `;
-const Header = styled.div``;
+const Header = styled.div`
+`;
 
 function Main(props) {
   return (
@@ -180,7 +182,7 @@ function Main(props) {
         </List>
         <List className="list">
           <Three id="MyBlog">
-            <ThreeDiv>my blog</ThreeDiv>
+            <ThreeDiv><NotificationList /></ThreeDiv>
           </Three>
         </List>
         <List className="list">
